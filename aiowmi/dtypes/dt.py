c57de894074d964d9f796245804661311b1cb0b6
Jeroen van der Heijden <jeroen@cesbit.com>
2022-02-22 10:36:42 +0100

    log warning and return dt from timestamp 0 on invalid datetime values

--- aiowmi/dtypes/dt.py
+++ aiowmi/dtypes/dt.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Union
 from datetime import datetime, timedelta
 
@@ -14,32 +15,42 @@ def dt_from_str(s: str) -> Union[datetime, timedelta]:
 
     if s.endswith(':000'):
         # ddddddddhhmmss.mmmmmm:000
-        return timedelta(
-            days=int(s[:8]),
-            hours=int(s[8:10]),
-            minutes=int(s[10:12]),
-            seconds=int(s[12:14]),
-            microseconds=int(s[15:21])
-        )
+        try:
+            td = timedelta(
+                days=int(s[:8]),
+                hours=int(s[8:10]),
+                minutes=int(s[10:12]),
+                seconds=int(s[12:14]),
+                microseconds=int(s[15:21])
+            )
+        except Exception as e:
+            logging.warning(f'invalid inteval value `{s}` ({e})')
+            td = timedelta(0)
+        return td
 
-    # timestamp: yyyymmddhhmmss.mmmmmmsutc
-    # utc is offset in minutes
-    minutes = int(s[-3:])
-    hours = minutes // 60
-    minutes = minutes % 60
+    try:
+        # timestamp: yyyymmddhhmmss.mmmmmmsutc
+        # utc is offset in minutes
+        minutes = int(s[-3:])
+        hours = minutes // 60
+        minutes = minutes % 60
 
-    for t in (4, 6):
-        # months and days must start at 1, not 0
-        e = t + 2
-        if s[t:e] == '00':
-            s = s[:t] + '01' + s[e:]
+        for t in (4, 6):
+            # months and days must start at 1, not 0
+            e = t + 2
+            if s[t:e] == '00':
+                s = s[:t] + '01' + s[e:]
 
-    dt = datetime.strptime(s[:21], _FMT)
-    if s[-4] == '+':
-        dt -= timedelta(hours=hours, minutes=minutes)
-    else:
-        assert s[-4] == '-'
-        dt += timedelta(hours=hours, minutes=minutes)
+        dt = datetime.strptime(s[:21], _FMT)
+        if s[-4] == '+':
+            dt -= timedelta(hours=hours, minutes=minutes)
+        else:
+            assert s[-4] == '-'
+            dt += timedelta(hours=hours, minutes=minutes)
+
+    except Exception as e:
+        logging.warning(f'invalid datetime value `{s}` ({e})')
+        dt = datetime.fromtimestamp(0)
     return dt
 
 
@@ -49,4 +60,6 @@ if __name__ == '__main__':
     print(dt_from_str('19980525183015.0000000+000'))
     print(dt_from_str('19980525******.0000000+000'))
     print(dt_from_str('1998**********.0000000+000'))
+    print(dt_from_str('00010000000000.0000000+300'))
+    print(dt_from_str('00000000000000.0000000+300'))
     print(dt_from_str('00000001132312.125***:000'))
